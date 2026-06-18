eae98d44fdd2a66844aaa0192a2e53b0496e1192
ProfessorITAman <tynysov.aman@gmail.com>
2026-06-18 17:25:29 +0600

    Update TimeSeriesResponse.kt

--- app/src/main/java/com/example/marketwatch/data/remote/dto/TimeSeriesResponse.kt
+++ app/src/main/java/com/example/marketwatch/data/remote/dto/TimeSeriesResponse.kt
@@ -3,6 +3,6 @@ package com.example.marketwatch.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class TimeSeriesResponse(
-    @SerializedName("Time Series (Daily)")  // ← Alpha Vantage JSON!
+    @SerializedName("Time Series (Daily)")
     val timeSeriesDaily: Map<String, TimeSeriesData>
 )
